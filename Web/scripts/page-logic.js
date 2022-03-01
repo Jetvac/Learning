5fa9982187cc4ce3dadd3c35211104842e3fbfc3
Jetvac <62834629+Jetvac@users.noreply.github.com>
2022-03-01 14:41:43 +0300

    Add tab button switch

--- Web/scripts/page-logic.js
+++ Web/scripts/page-logic.js
@@ -1,16 +1,30 @@
 $(document).ready(function () {
-    function openTab(tabName) {
+    function deleteClassByClassName(className, deletedClass) {
         var i, tabcontent;
-            
-        tabcontent = document.getElementsByClassName("active-place__tab");
+
+        tabcontent = document.getElementsByClassName(className);
         for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].className = tabcontent[i].className.replace(" active-tab", "");
+            tabcontent[i].className = tabcontent[i].className.replace(" " + deletedClass, "");
         }
+    }
+
+    function openTab(tabName) {
+        deleteClassByClassName("active-place__tab", "active-tab");
         
         document.getElementById(tabName).className += " active-tab";
     }
+    function setButtonActive(buttonName) {
+        deleteClassByClassName("menu-panel__button", "tab-button_active");
+
+        document.getElementById(buttonName).className += " tab-button_active";
+    }
+
     $('#courses-button').click(function(e) {
+        setButtonActive('courses-button');
         openTab('course-panel');
+    });
+    $('#courses-button-1').click(function(e) {
+        setButtonActive('courses-button-1');
     });
     $('#add-course__navigate-button').click(function(e) {
         openTab('course-add-panel');
